371a8bfb1d352b8e5b36e8b871c19f8b978dd04d
victordek <victordek@yandex.ru>
2017-02-12 13:20:54 +0300

    task_7 Done

--- task_7.py
+++ task_7.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
-
+import roborders as rd
 
 @task
 def task_5_4():
-    pass
+    #the boarder of the field
+    rd.move_along_the_wall('down', 'left')
+    #the wall
+    rd.move_along_the_wall('right', 'down')
+    #around the end of the wall
+    move_down()
+    move_left()
+    #to the exit
+    rd.move_along_the_wall('left', 'up')
 
 
 if __name__ == '__main__':
