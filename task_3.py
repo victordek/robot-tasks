ab27a9ce2ed98e11d13d58af65977a32a144d1a8
victordek <victordek@yandex.ru>
2017-02-12 11:09:30 +0300

    Update task3 to use roborders

--- task_3.py
+++ task_3.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
-
+import roborders as rd
 
 @task
 def task_3_1():
-    while not wall_is_on_the_right():
-    	move_right()
-
+    rd.move_to_wall
 
 if __name__ == '__main__':
     run_tasks()
