5d9b605ea9ed30a31145898eea807448c3dc18c2
victordek <victordek@yandex.ru>
2017-02-15 20:24:22 +0300

    task_20 Done

--- task_20.py
+++ task_20.py
@@ -2,11 +2,20 @@
 
 from pyrob.api import *
 
+x_size = 27
+y_size = 12
 
 @task(delay=0.05)
 def task_4_3():
-    pass
-
+    move_right()
+    for i in range(y_size):
+        
+        for j in range(x_size):
+            fill_cell()
+            move_right()
+        move_left(x_size)
+        
+        move_down()
 
 if __name__ == '__main__':
     run_tasks()
