41e9845a2274bb79cc26e167e277c1af26761f89
victordek <victordek@yandex.ru>
2017-02-15 23:16:24 +0300

    task_31 Done

--- task_31.py
+++ task_31.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
+import roborders as rd
+
 
 
 @task(delay=0.01)
 def task_8_30():
-    pass
+    while True:
+        rd.move_to_wall('right')
+        rd.move_along_the_wall('left', 'down')
+        if not wall_is_beneath():
+            rd.move_to_wall('down')
+            continue
+        else:
+            return
 
 
 if __name__ == '__main__':
