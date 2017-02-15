c17f4b26f7b9d8c4b8d1fe1f27003832267b53b4
victordek <victordek@yandex.ru>
2017-02-15 22:25:22 +0300

    task_29 Done

--- task_29.py
+++ task_29.py
@@ -2,10 +2,17 @@
 
 from pyrob.api import *
 
-
 @task
 def task_7_7():
-    pass
+    count = 0
+    while not wall_is_on_the_right():
+        if cell_is_filled():
+            count += 1
+        else:
+            count = 0
+        if count == 3:
+            return
+        move_right()
 
 
 if __name__ == '__main__':
