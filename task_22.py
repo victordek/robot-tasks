546f722fec62ab8cddd97865602c447737e6c22f
victordek <victordek@yandex.ru>
2017-02-15 21:16:54 +0300

    task_22 Done

--- task_22.py
+++ task_22.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
-
+import roborders as rd
 
 @task
 def task_5_10():
-    pass
-
+    while not wall_is_beneath():
+        rd.filling_the_line_from_end_to_end('right')
+        move_down()
+    rd.filling_the_line_from_end_to_end('right')
 
 if __name__ == '__main__':
     run_tasks()
