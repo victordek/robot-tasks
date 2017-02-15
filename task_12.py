a3f49104b08de360d10a6930716560c04b863267
victordek <victordek@yandex.ru>
2017-02-15 17:37:17 +0300

    task_12 Done

--- task_12.py
+++ task_12.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
+import roborders as rd
 
+def condition (wall_above, wall_beneath):
+    return not wall_above() and wall_beneath()
 
 @task
 def task_8_6():
-    pass
-
+    rd.action_in_the_hall(condition)
 
 if __name__ == '__main__':
     run_tasks()
