849f1329dd31bf5f65d2d0fe8fb392a211df21dc
victordek <victordek@yandex.ru>
2017-02-15 21:45:40 +0300

    task_25 Done

--- task_25.py
+++ task_25.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
-
+import roborders as rd
 
 @task
 def task_2_2():
-    pass
+    move_down()
+    rd.draw_cross()
+    for i in range (4):
+        move_right(4)
+        rd.draw_cross()
 
 
 if __name__ == '__main__':
