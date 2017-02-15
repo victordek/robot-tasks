189219b6b5c2c15b6a080f58c6be64498d3d00f3
victordek <victordek@yandex.ru>
2017-02-15 21:53:32 +0300

    task_26 Done

--- task_26.py
+++ task_26.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
+import roborders as rd
 
+def draw_line_of_crosses_and_back (number = 10):
+    rd.draw_cross()
+    for i in range(number - 1):
+        move_right(4)
+        rd.draw_cross()
+    rd.move_to_wall('left')
 
 @task(delay=0.02)
 def task_2_4():
-    pass
+    draw_line_of_crosses_and_back()
+    for i in range(4):
+        move_down(4)
+        draw_line_of_crosses_and_back()
 
 
 if __name__ == '__main__':
