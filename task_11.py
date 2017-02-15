3a34218d4c982ab09d85639338f1a7d1fd58e180
victordek <victordek@yandex.ru>
2017-02-15 16:15:25 +0300

    task_11 Done

--- task_11.py
+++ task_11.py
@@ -5,8 +5,15 @@ from pyrob.api import *
 
 @task
 def task_8_4():
-    pass
+    #check the starting place for filling
+    if wall_is_above() and wall_is_beneath():
+        fill_cell()
 
+    while not wall_is_on_the_right():
+        move_right()
+        # checking new position for filling
+        if wall_is_above() and wall_is_beneath():
+            fill_cell()
 
 if __name__ == '__main__':
     run_tasks()
