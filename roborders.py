d2077af5fce730541125832eb06d8b1fad2f3eb7
victordek <victordek@yandex.ru>
2017-02-15 13:01:51 +0300

    roborders reworked to have clear parametres' names

--- roborders.py
+++ roborders.py
@@ -20,28 +20,25 @@ wall_functions = {
 def move_to_wall (direct = 'right'):
     '''Moving the robot in the given direction till the wall
 	direct: ['right', 'leaft', 'up', 'down']'''
-    move_direct, check_wall = moving_functions[direct], wall_functions[direct]
+    move_direct, wall_on_the_way = moving_functions[direct], wall_functions[direct]
     while not check_wall():
         move_direct()
 
-def move_along_the_wall (direct, wall_on_the_side):
+def move_along_the_wall (direct, wall_on_side_direct):
     '''Moving along the wall on the side in the given direction.
-    Checking that wall is on the side than moving. So stops after the wall
+    Checking that wall is on the side than moving. So stops in the next point from the wall`s end
     direct: ['right', 'leaft', 'up', 'down']
-    wall_on_the_side: ['right', 'leaft', 'up', 'down']'''
+    wwall_on_side_direct: ['right', 'leaft', 'up', 'down']'''
     if direct == wall_on_the_side:
         print ("Direrctions shouldn`t be the same")
         return
 
-    # move_direct contains function to move
-    move_direct = moving_functions[direct]
-    # check_move_direct contains checker of the direction
-    check_move_direct = wall_functions[direct]
-    # check_side_wall contains the side to check the wall
-    check_side_wall = wall_functions[wall_on_the_side]
+    move_direct, wall_on_the_way = moving_functions[direct], wall_functions[direct]
 
-    while check_side_wall():
-        if not check_move_direct():
+    wall_on_the_side = wall_functions[wall_on_side_direct]
+
+    while wall_on_the_side():
+        if not wall_on_the_way():
             move_direct()
         else:
             # stop function and prevent smashing in to the corner
