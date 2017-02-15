394f6e25118a879d8cb11733602b6d27717d82c3
victordek <victordek@yandex.ru>
2017-02-15 19:30:10 +0300

    task_14 Done

--- task_14.py
+++ task_14.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
+def check_and_fill():
+    if not wall_is_above():
+        move_up()
+        fill_cell()
+        move_down()
+
+    if not wall_is_beneath():
+        move_down()
+        fill_cell()
+        move_up()
+    if wall_is_above() and wall_is_beneath():
+        fill_cell()
 
 
 @task
 def task_8_11():
-    pass
+    check_and_fill()
 
+    while not wall_is_on_the_right():
+        move_right()
+        check_and_fill()
 
 if __name__ == '__main__':
     run_tasks()
