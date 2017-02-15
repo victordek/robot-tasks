d4d57044c492a7f7c5395a87fc37f3edf174be8d
victordek <victordek@yandex.ru>
2017-02-15 20:00:56 +0300

    task_17 Done

--- task_17.py
+++ task_17.py
@@ -5,7 +5,11 @@ from pyrob.api import *
 
 @task
 def task_8_27():
-    pass
+    while not cell_is_filled():
+        move_up()
+    move_left()
+    if not cell_is_filled():
+        move_right(2)
 
 
 if __name__ == '__main__':
