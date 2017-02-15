b34385d2004c2417381526706c293c847691df77
victordek <victordek@yandex.ru>
2017-02-15 23:07:25 +0300

    task_30 Done

--- task_30.py
+++ task_30.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
+import roborders as rd
+
 
 
 @task(delay=0.01)
 def task_9_3():
-    pass
+    # count the length
+    length = 1
+    while not wall_is_on_the_right():
+        move_right()
+        length += 1
+    print (length)
+
+    rd.draw_triangle(length, 'down', 'left')
+    rd.draw_triangle(length, 'left', 'down')
+    rd.move_to_the_opposite_corner()
+    rd.draw_triangle(length, 'up', 'right')
+    rd.draw_triangle(length, 'right', 'up')
 
 
 if __name__ == '__main__':
